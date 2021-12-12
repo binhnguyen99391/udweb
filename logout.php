555ac323c58dbf873383edf547c9e5ed0154b238
binhnt <binhnguyen99391@gmail.com>
2021-12-12 09:53:52 +0700

    delete

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    if (session_destroy()) {
-      unset($_SESSION["username"]);
-      $_SESSION['loggedin'] = false;
-      header("Location: auth/login.php");
-    }
